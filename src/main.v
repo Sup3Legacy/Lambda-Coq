f8b3271ec599b96eaabaf637fc6bb5d6a8955e75
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2021-04-17 15:48:57 +0200

    Changed substitution definition

--- src/main.v
+++ src/main.v
@@ -22,16 +22,24 @@ Definition var_less_equal_n (T: DeBruijn) (n: nat) :=
 Definition terme_clos (T: DeBruijn) :=
     var_less_equal_n T 0.
 
-Fixpoint substitution (T: DeBruijn) (index: nat) (S: DeBruijn) :=
+(* mini is the minimal index of a free variable, that can be substituted *)
+Fixpoint substitution_aux (T: DeBruijn) (index: nat) (mini: nat) (S: DeBruijn) :=
     match T with
     | Var n =>
         match (Nat.eqb index n) with 
-        | true => S
+        | true => 
+            match (Nat.leb mini index) with 
+            | true => S
+            | false => Var n
+            end
         | false => Var n
         end
-    | Lambda tp => Lambda (substitution tp index S)
-    | Application tp0 tp1 => Application (substitution tp0 index S) (substitution tp1 index S)
+    | Lambda tp => Lambda (substitution_aux tp index (mini + 1) S)
+    | Application tp0 tp1 => Application (substitution_aux tp0 index mini S) (substitution_aux tp1 index mini S)
     end.
+
+Definition substitution (T: DeBruijn) (index: nat) (S: DeBruijn) :=
+    substitution_aux T index 0 S.
 
 Theorem substitution_clos: forall T: DeBruijn, 
     terme_clos T -> forall index: nat, forall S: DeBruijn, 
@@ -39,13 +47,9 @@ Theorem substitution_clos: forall T: DeBruijn,
 Proof.
     intros.
     induction T.
+Qed.
 
 Lemma recurrence_var_less: forall T: DeBruijn, forall n: nat,
     var_less_equal_n T n -> var_less_equal_n T (n + 1).
-Proof.
-    intro.
-    intro.
-    intro.
-    destruct H.
 
 
